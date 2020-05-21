45fb751c2bf9ce2c6d0e11836f1afd17e6db509c
pieos-pi <pieos.builder.pi@gmail.com>
2020-05-21 12:59:29 +0900

    PIEOS, PIE token symbol

---
+++ contracts/include/pieos.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+
+///////////////////////////////////
+/// EOS Native
+
+#define EOS_SYSTEM_CONTRACT "eosio"_n
+#define EOS_TOKEN_CONTRACT "eosio.token"_n
+
+#define EOS_SYMBOL_STR "EOS"
+#define EOS_SYMBOL_DECIMAL 4
+#define EOS_SYMBOL eosio::symbol(EOS_SYMBOL_STR,EOS_SYMBOL_DECIMAL)
+
+#define REX_SYMBOL_STR "REX"
+#define REX_SYMBOL_DECIMAL 4
+#define REX_SYMBOL eosio::symbol(REX_SYMBOL_STR,REX_SYMBOL_DECIMAL)
+
+
+///////////////////////////////////
+/// PIEOS Governance Token
+
+#define PIEOS_TOKEN_CONTRACT_STR "pieostokenct"
+#define PIEOS_TOKEN_CONTRACT PIEOS_TOKEN_CONTRACT_STR_n
+
+#define PIEOS_SYMBOL_STR "PIEOS"
+#define PIEOS_SYMBOL_DECIMAL 4
+#define PIEOS_SYMBOL eosio::symbol(PIEOS_SYMBOL_STR,PIEOS_SYMBOL_DECIMAL)
+
+
+///////////////////////////////////
+/// PIEOS SCO(Stake-Coin-Offering) Governance Token Distribution
+
+#define PIEOS_SCO_CONTRACT_STR "scopieosdist"
+
+
+///////////////////////////////////
+/// PIE Stablecoin
+
+#define PIE_TOKEN_CONTRACT_STR "piestbltoken"
+#define PIE_TOKEN_CONTRACT PIE_TOKEN_CONTRACT_STR_n
+
+#define PIE_SYMBOL_STR "PIE"
+#define PIE_SYMBOL_DECIMAL 4
+#define PIE_SYMBOL eosio::symbol(PIE_SYMBOL_STR,PIE_SYMBOL_DECIMAL)
+
+
